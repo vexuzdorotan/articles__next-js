8fc2051f88dc37138a21710441424bdc86325eb9
VeXuZ <vexuzdorotan@gmail.com>
2021-03-04 09:38:37 +0800

    init, practice next.js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from '../components/Meta'
+
+const about = () => {
+  return (
+    <div>
+      <Meta title='Abouttt' />
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default about
